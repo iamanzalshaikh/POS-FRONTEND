578850263ea739c1cc849d0a558cf968fc6901c7
Aftab784 <af227656@gmail.com>
2026-04-27 15:59:19 +0500

    Fixed the invoice number

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-ca84f546'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.lbn7oe6j47o"
+    "revision": "0.35d65ld69ko"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
